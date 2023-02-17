34982db74a4f99182c606ec8d57987add6721a9b
HwangInUng <ung6860@naver.com>
2023-02-17 17:48:24 +0900

    android Rest 요청 구현

--- SpringBoard/src/main/java/com/edu/springboard/controller/client/NoticeController.java
+++ SpringBoard/src/main/java/com/edu/springboard/controller/client/NoticeController.java
@@ -1,28 +1,51 @@
 package com.edu.springboard.controller.client;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.springboard.domain.Notice;
+import com.edu.springboard.exception.NoticeException;
 import com.edu.springboard.model.notice.NoticeService;
 
 @RestController
+@RequestMapping("/rest/notice")
 public class NoticeController {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private NoticeService noticeService;
 	
-	@PostMapping("/rest/notice/regist")
+	@PostMapping("/regist")
 	public String regist(Notice notice) {
 		log.info(notice.getTitle());
 		log.info(notice.getWriter());
 		log.info(notice.getContent());
 		
-		return "post test";
+		noticeService.regist(notice);
+		
+		return "등록 성공";
+	}
+	
+	@GetMapping("/list")
+	public List getList() {
+		return noticeService.selectAll();
+	}
+	
+	@GetMapping("/detail")
+	public Notice getDetail(int notice_idx) {
+		return noticeService.detail(notice_idx);
+	}
+	
+	@ExceptionHandler(NoticeException.class)
+	public String handle(NoticeException e) {
+		return e.getMessage();
 	}
 }
